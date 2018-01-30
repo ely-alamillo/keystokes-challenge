8c1223409ab8098e422f3238304fc8f4b5ef3e6e
Ely Alamillo <ely0013@gmail.com>
2018-01-29 22:01:11 -0600

    inital server config

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+const dotenv = require('dotenv').config();
+const logger = console;
+
+const corsOptions = {
+  origin: true,
+  methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+  preflightContinue: true,
+  optionsSuccessStatus: 204,
+  credentials: true // enable set cookie
+};
+
+const server = express();
+
+// server options for receiving/sending data
+server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json());
+server.use(cors(corsOptions));
+
+// static server for build
+server.use(express.static(path.join(__dirname, 'client/build')));
+
+const connect = mongoose.connect(process.env.MONGODB_URI, {
+  useMongoClient: true
+});
+
+connect
+  .then(() => {
+    const { routes } = require('./api/routes/routes');
+    routes(server);
+    server.listen(process.env.PORT);
+    logger.log('connection to MongoDB successfull...');
+    logger.log(`server listening on port: ${process.env.PORT}`);
+  })
+  .catch(() => {
+    logger.log('error connecting to MongoDB');
+  });
