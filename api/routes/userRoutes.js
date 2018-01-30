10ffd07d3fc08dfd3a4010dd1000cc1eccff0460
Ely Alamillo <ely0013@gmail.com>
2018-01-29 23:45:34 -0600

    wired up user routes

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -2,7 +2,20 @@ const express = require('express');
 const userControllers = require('../controllers/userControllers');
 
 const routes = server => {
-  server.route('/test').get(userControllers.hello);
+  // API Routes
+  const api = express.Router();
+
+  api.route('/test').get(userControllers.hello);
+  api.route('/register').post(userControllers.register);
+  api.route('/login').post(userControllers.login);
+  api.route('/update').post(userControllers.update);
+  api.route('/users').post(userControllers.allUsers);
+  server.use('/api', api);
+
+  // catch all for unknown routes
+  server.route('*', (req, res) => {
+    res.redirect('/');
+  });
 };
 
 module.exports = { routes };
