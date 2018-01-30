2a092c6fd19d6cadee65e97e991ac483e1f8cee8
Ely Alamillo <ely0013@gmail.com>
2018-01-29 22:40:37 -0600

    used better linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,27 @@ module.exports = {
     browser: true,
     commonjs: true,
     es6: true,
-    node: true,
+    node: true
   },
   // extends: "airbnb-base",
-  extends: "eslint:recommended",
+  // extends: ["eslint:recommended", "plugin:react/recommended"],
+  extends: ['eslint:recommended'],
   parserOptions: {
     ecmaFeatures: {
       experimentalObjectRestSpread: true,
+      jsx: true
     },
-    sourceType: "module"
+    sourceType: 'module'
   },
+  plugins: ['react'], // use only with react
   rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "always"],
-    "object-curly-spacing": ["error", "always"],
-    "no-unused-vars": ["error", {"varsIgnorePattern": "dotenv"}]
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'object-curly-spacing': ['error', 'always'],
+    'no-unused-vars': ['error', { varsIgnorePattern: 'dotenv' }],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error'
   }
 };
