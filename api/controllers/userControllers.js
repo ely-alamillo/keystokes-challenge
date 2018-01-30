1fee4aec950f9483b339eef0eb0e39dcacc2c7af
Ely Alamillo <ely0013@gmail.com>
2018-01-29 23:46:08 -0600

    added register, login, update, allUsers controllers

--- api/controllers/userControllers.js
+++ api/controllers/userControllers.js
@@ -2,8 +2,22 @@ const User = require('../models/userModel');
 const sendUserError = require('../Helpers');
 
 const register = (req, res) => {
-  const { username, firstName, lastName, description, email } = req.body;
-  if (!username || !firstName || lastName || description || email) {
+  const {
+    username,
+    firstName,
+    lastName,
+    description,
+    email,
+    profileImg
+  } = req.body;
+  if (
+    !username ||
+    !firstName ||
+    !lastName ||
+    !description ||
+    !email ||
+    !profileImg
+  ) {
     return sendUserError('Please provide all information');
   }
   const newUser = new User({
@@ -15,7 +29,37 @@ const register = (req, res) => {
   });
   newUser.save((err, savedUser) => {
     if (err) return sendUserError(err, res);
-    return res.json({ saved: true });
+    return res.json({ saved: true, savedUser });
+  });
+};
+
+const login = (req, res) => {
+  const { email } = req.body;
+  if (!email) return sendUserError('An Error occured please try again', res);
+  User.findOne({ email }, (err, user) => {
+    if (err) return sendUserError(err, res);
+    return res.json(user);
+  });
+};
+
+const update = (req, res) => {
+  const { firstName, lastName, description, id, profileImg } = req.body;
+  if (!firstName || !lastName || !description || !profileImg) {
+    return sendUserError('Please provide all info', res);
+  }
+  const update = { firstName, lastName, description, profileImg };
+  User.findOneByIdAndUpdate(id, update, { new: true }, (err, updated) => {
+    if (err) {
+      return sendUserError('An error occurred please try again', res);
+    }
+    return res.json({ changed: true, updated });
+  });
+};
+
+const allUsers = (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) return sendUserError(err, res);
+    return res.json(users);
   });
 };
 
@@ -23,4 +67,4 @@ const hello = (req, res) => {
   return res.json({ msg: 'hello from the api' });
 };
 
-module.exports = { register, hello };
+module.exports = { register, hello, login, update, allUsers };
