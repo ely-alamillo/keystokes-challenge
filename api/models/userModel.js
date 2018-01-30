40fe3b80fa464387c8989f8bf322105d75ab2484
Ely Alamillo <ely0013@gmail.com>
2018-01-29 23:46:26 -0600

    added key for profileimg

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
     description: { type: String, required: true },
-    email: { type: String, required: true }
+    email: { type: String, required: true, unique: true },
+    profileImg: { type: String, required: true }
   },
   { timestamps: true }
 );
