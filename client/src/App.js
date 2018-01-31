6c261c8c06cc3ae0a98f373b1a065afa410070f4
Ely Alamillo <ely0013@gmail.com>
2018-01-31 00:00:01 -0600

    added dashboard route

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,26 @@ import { Route } from 'react-router-dom';
 import View from './components/View/View';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedIn: false
+    };
+  }
   render() {
     return (
       <MuiThemeProvider>
         <View />
-        <Route path="/" exact component={Login} />
+        <Route
+          path="/"
+          exact
+          component={() => <Dashboard test="thisIsATest" />}
+        />
+        <Route path="/login" exact component={Login} />
         <Route path="/register" exact component={Register} />
       </MuiThemeProvider>
     );
