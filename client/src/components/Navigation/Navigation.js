4b5f7febc84a01718822e526a0cd5575fd631384
Ely Alamillo <ely0013@gmail.com>
2018-01-30 20:46:13 -0600

    removed test file and service worker

---
+++ client/src/components/Navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+import './App.css';
+
+const Navigation = props => {
+  <AppBar
+    title="Title"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+  />;
+};
+
+export default Navigation;
