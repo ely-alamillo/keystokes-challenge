90e7adc2b22374d57d824ec28343fdb427dde63e
Ely Alamillo <ely0013@gmail.com>
2018-01-31 01:01:23 -0600

    switched to stateless component

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -4,33 +4,67 @@ import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import DrawerLeft from '../Drawer/Drawer';
 
-export default class AppBarTop extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      open: false
-    };
-  }
-  //Toggle function (open/close Drawer)
-  toggleDrawer() {
-    this.setState({
-      open: !this.state.open
-    });
-  }
+// export default class AppBarTop extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       open: false
+//     };
+//   }
 
-  render() {
-    return (
-      <div>
-        <AppBar
-          title="Title"
-          onClick={this.toggleDrawer.bind(this)}
-          iconElementRight={<FlatButton label="Login" />}
-        />
-        <DrawerLeft
-          open={this.state.open}
-          onToggleDrawer={this.toggleDrawer.bind(this)}
-        />
-      </div>
-    );
-  }
-}
+//   //Toggle function (open/close Drawer)
+//   toggleDrawer() {
+//     this.setState({
+//       open: !this.state.open
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <AppBar
+//           title="Title"
+//           onClick={this.toggleDrawer.bind(this)}
+//           iconElementRight={
+//             this.state.loggedIn ? (
+//               <FlatButton label="Logout" onClick={this.props.toggle} />
+//             ) : (
+//               <h3>hello</h3>
+//             )
+//           }
+//         />
+//         <DrawerLeft
+//           open={this.state.open}
+//           onToggleDrawer={this.toggleDrawer.bind(this)}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+const AppBarTop = props => {
+  return (
+    <div>
+      <AppBar
+        title="Title"
+        onTitleClick={props.toggleDrawer}
+        onLeftIconButtonClick={props.toggleDrawer}
+        onRightIconButtonClick={props.toggle}
+        iconElementRight={
+          props.loggedIn ? (
+            <FlatButton label="Logout" />
+          ) : (
+            <FlatButton label="Login" />
+          )
+        }
+      />
+      <DrawerLeft
+        open={props.open}
+        toggleDrawer={props.toggleDrawer}
+        test="test"
+      />
+    </div>
+  );
+};
+
+export default AppBarTop;
