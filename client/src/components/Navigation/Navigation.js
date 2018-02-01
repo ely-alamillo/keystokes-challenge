8e256dad878fb041675804d78d18164f65603b21
Ely Alamillo <ely0013@gmail.com>
2018-02-01 16:23:48 -0600

    added styles

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,52 +1,13 @@
 import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
-import RaisedButton from 'material-ui/RaisedButton';
 import DrawerLeft from '../Drawer/Drawer';
-
-// export default class AppBarTop extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       open: false
-//     };
-//   }
-
-//   //Toggle function (open/close Drawer)
-//   toggleDrawer() {
-//     this.setState({
-//       open: !this.state.open
-//     });
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <AppBar
-//           title="Title"
-//           onClick={this.toggleDrawer.bind(this)}
-//           iconElementRight={
-//             this.state.loggedIn ? (
-//               <FlatButton label="Logout" onClick={this.props.toggle} />
-//             ) : (
-//               <h3>hello</h3>
-//             )
-//           }
-//         />
-//         <DrawerLeft
-//           open={this.state.open}
-//           onToggleDrawer={this.toggleDrawer.bind(this)}
-//         />
-//       </div>
-//     );
-//   }
-// }
 
 const AppBarTop = props => {
   return (
     <div>
       <AppBar
-        title="Title"
+        title="Keystoke Users"
         onTitleClick={props.toggleDrawer}
         onLeftIconButtonClick={props.toggleDrawer}
         onRightIconButtonClick={props.signout}
