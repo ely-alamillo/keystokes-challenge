ea54beb159af7bb06fcca586ef7df58771ae5596
Ely Alamillo <ely0013@gmail.com>
2018-01-31 22:58:44 -0600

    was messing with react router, nothing changed

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <MuiThemeProvider>
+    <Router>
+      <App />
+    </Router>
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
